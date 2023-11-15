9a0b127598b88e9499cd20e96fc9cc3ed911c847
Sahar <saharhalim151@gmail.com>
2023-11-14 22:00:45 -0500

    define views and auth

--- website/views.py
+++ website/views.py
@@ -1,3 +1,5 @@
 from flask import Blueprint
 
-views = Blueprint('views', __name__) # this file is blueprint of our application which means their are some route here which we can organize in different files, names can be changed views in blueprint function can be anything else as well
+views = Blueprint('views', __name__) 
+#variable name view can be anything
+# this file is blueprint of our application which means their are some route here which we can organize in different files, names can be changed views in blueprint function can be anything else as well
