771f66b66c360ad98653166c1d0227fbb819f028
Sahar <saharhalim151@gmail.com>
2023-11-15 11:27:05 -0500

    add routes in main

--- website/views.py
+++ website/views.py
@@ -3,3 +3,7 @@ from flask import Blueprint
 views = Blueprint('views', __name__) 
 #variable name view can be anything
 # this file is blueprint of our application which means their are some route here which we can organize in different files, names can be changed views in blueprint function can be anything else as well
+
+@views.route('/')
+def home():
+  return "<h1> Test </h2>"
